4e8a477d5aa3fd32cbb0b4fd98fe32d09f323f3c
hae-on <solbi2004@naver.com>
2021-06-21 18:54:15 +0900

    랜덤으로 명대사, 배경이미지 삽입

---
+++ js/background.js
@@ -0,0 +1,14 @@
+const images = ["0.jpg", "1.jpg", "2.jpeg", "3.jpeg", "4.jpeg"];
+
+const randomBg = images[Math.floor(Math.random() * images.length)];
+
+// img는 이미지가 담겨있는 폴더 이름 (queryselector 같은 느낌)
+const bgImage = document.createElement("img");
+
+// "img/1.jpg , img/3.heic" 처럼 출력하기 위해
+bgImage.src = `img/${randomBg}`;
+
+// html에 bgImage를 추가해준다. 랜덤하게 바뀌기에 html에 직접 넣어주진 못함
+document.body.appendChild(bgImage);
+
+console.log(bgImage);
---
+++ js/quotes.js
@@ -0,0 +1,52 @@
+const quotes = [
+  {
+    quote: "To infinity, and beyond!",
+    movie: "Toy-story",
+  },
+  {
+    quote:
+      "The happiest man on earth would look into the mirror and see only himself exactly as he is. ",
+    movie: "Harry Potter",
+  },
+  {
+    quote: "Sometimes the right path is not the easiest one ",
+    movie: "Pocahontas",
+  },
+  {
+    quote: "Remember, you are the one who can fill the world with sunshine",
+    movie: "Snow white",
+  },
+  {
+    quote: "Some people are worth melting for",
+    movie: "Frozen 2",
+  },
+  {
+    quote:
+      "If you don't know where you want to go, then it doesn't matter which path you take.",
+    movie: "Alice in the wonderland",
+  },
+  { quote: "To make each day count", movie: "Titanic" },
+  {
+    quote: "Knowing is the easy part, saying it out loud is the hard part",
+    movie: "Laboum",
+  },
+  {
+    quote:
+      "When you do decide to try it, it won't be any good. You shold take a chance. Got nothing to lose",
+    movie: "Home Alone",
+  },
+  {
+    quote:
+      "The past can hurt. But the way i see it, you can either run from it or learn from it",
+    movie: "Lion King",
+  },
+];
+
+// "#quote span:first-child" 에서 :과 first 뒤에 띄어쓰기 금지
+const quote = document.querySelector("#quote span:first-child");
+const movie = document.querySelector("#quote span:last-child");
+
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todayQuote.quote;
+movie.innerText = todayQuote.movie;
